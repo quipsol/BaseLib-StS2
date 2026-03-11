692b324a506d8f56c17f2e4f757eba7ee151d03e
ocean <ocean@ocean.lol>
2026-03-11 23:17:24 +0000

    move patches to potion model file

--- Abstracts/CustomPotionModel.cs
+++ Abstracts/CustomPotionModel.cs
@@ -1,4 +1,4 @@
-using BaseLib.Patches;
+using HarmonyLib;
 using BaseLib.Patches.Content;
 using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Models;
@@ -15,4 +15,25 @@ public abstract class CustomPotionModel : PotionModel, ICustomModel
 
     public virtual string PackedImagePath => ImageHelper.GetImagePath("atlases/potion_atlas.sprites/fire_potion.tres");
     public virtual string PackedOutlinePath => ImageHelper.GetImagePath("atlases/potion_outline_atlas.sprites/fire_potion.tres");
+    
+    [HarmonyPatch(typeof(PotionModel), nameof(CustomPotionModel.PackedImagePath), MethodType.Getter)]
+    private static class ImagePatch {
+        static bool Prefix(PotionModel __instance, ref string __result) {
+            if (__instance is CustomPotionModel model) {
+                __result = model.PackedImagePath;
+                return false;
+            }
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(PotionModel), nameof(CustomPotionModel.PackedOutlinePath), MethodType.Getter)]
+    private static class OutlinePatch {
+        static bool Prefix(PotionModel __instance, ref string __result) {
+            if (__instance is CustomPotionModel model) {
+                __result = model.PackedOutlinePath;
+                return false;
+            }
+            return true;
+        }
+    }
 }
